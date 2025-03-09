dda8157ab5968d4220493d1ffba46fe8cf1382e6
BVancea25 <b_vancea@yahoo.com>
2025-03-09 22:30:26 +0200

    added CR for currencies

--- src/main/java/com/dataflow/dataingestionservice/Controllers/CurrencyController.java
+++ src/main/java/com/dataflow/dataingestionservice/Controllers/CurrencyController.java
@@ -2,6 +2,7 @@ package com.dataflow.dataingestionservice.Controllers;
 
 
 import com.dataflow.dataingestionservice.Config.TransactionBatchConfig;
+import com.dataflow.dataingestionservice.Models.Currency;
 import com.dataflow.dataingestionservice.Services.CurrencyService;
 import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
@@ -13,14 +14,11 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @RestController
 public class CurrencyController {
@@ -72,5 +70,20 @@ public class CurrencyController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("File processing failed: " + e.getMessage());
         }
+    }
+
+    @GetMapping("/currency")
+    public List<Currency> getCurrencies(){
+        return currencyService.getCurrencies();
+    }
+
+    @GetMapping("/currency")
+    public Optional<Currency> getCurrency(@RequestParam("id") UUID uuid){
+        return currencyService.getCurrencyById(uuid);
+    }
+
+    @GetMapping
+    public Currency getCurrency(@RequestParam("code") String code){
+        return currencyService.getCurrencyByCode(code);
     }
 }
--- src/main/java/com/dataflow/dataingestionservice/Services/CurrencyService.java
+++ src/main/java/com/dataflow/dataingestionservice/Services/CurrencyService.java
@@ -1,7 +1,29 @@
 package com.dataflow.dataingestionservice.Services;
 
+import com.dataflow.dataingestionservice.Models.Currency;
+import com.dataflow.dataingestionservice.Repositories.CurrencyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CurrencyService {
+
+    @Autowired
+    CurrencyRepository currencyRepository;
+
+    public List<Currency> getCurrencies(){
+        return currencyRepository.findAll();
+    }
+
+    public Optional<Currency> getCurrencyById(UUID id){
+        return currencyRepository.findById(id);
+    }
+
+    public Currency getCurrencyByCode(String code){
+        return currencyRepository.findByCode(code);
+    }
 }
